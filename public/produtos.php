a5762517d21fa12ea60f20b6edb53729109c768a
Rhuan Cesar <rhuan.silva@anestesiacarioca.com.br>
2023-04-03 12:00:02 -0300

    Create e read - Produtos - html1

--- public/produtos.php
+++ public/produtos.php
@@ -5,12 +5,13 @@ require "./protect.php";
 /* Read */
 $nome = $_POST['buscar'] ?? '';
 $SQL = "SELECT * FROM produtos WHERE nome LIKE '%$nome%' AND vigente = 'S';";
-$result = $mysqli->query($SQL); 
+$result = $mysqli->query($SQL);
 
 ?>
 
 <!doctype html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,6 +23,7 @@ $result = $mysqli->query($SQL);
   <link rel="stylesheet" href="../assets/css/home.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
+
 <body>
   <nav class="site-header sticky-top py-1 d-flex">
     <div class="container d-flex flex-column flex-md-row justify-content-around">
@@ -31,40 +33,39 @@ $result = $mysqli->query($SQL);
     </div>
     <button type="button" class="btn btn-light m-1"><a href="logout.php" style="color:#000; text-decoration:none;">Sair</a></button>
   </nav>
-  <div class="card col-12 mb-3">
+  <div class="page-inner m-2">
+    <div class="card col-12 mb-3 mt-3">
       <div class="card-header">
         <h5 class="card-title">CADASTRAR</h5>
       </div>
-    <div class="card-body">
-      <form action="adicionar.php" method="post">
-        <input for="id" type="hidden" class="form-control" id="id" name="id" value="">
-        <div class="col-10 d-flex">
-          <div class="col-md-5 m-2">
-            <label for="produto" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="produto" name="nome" value="" required="" autofocus>
+      <div class="card-body">
+        <form action="adicionar.php" method="post">
+          <input for="id" type="hidden" class="form-control" id="id" name="id" value="">
+          <div class="col-10 d-flex">
+            <div class="col-md-5 m-2">
+              <label for="produto" class="form-label">Nome</label>
+              <input type="text" class="form-control" id="produto" name="nome" value="" required="" autofocus>
+            </div>
+            <div class="col-md-8 m-2">
+              <label for="produto" class="form-label">Descrição</label>
+              <input type="text" class="form-control" id="produto" name="descri" value="" required="">
+            </div>
+            <div class="col-md-1 m-2">
+              <label for="produto" class="form-label">Quantidade</label>
+              <input type="text" class="form-control" id="produto" name="qnt" value="" required="">
+            </div>
           </div>
-          <div class="col-md-8 m-2">
-            <label for="produto" class="form-label">Descrição</label>
-            <input type="text" class="form-control" id="produto" name="descri" value="" required="">
-          </div>
-          <div class="col-md-1 m-2">
-            <label for="produto" class="form-label">Quantidade</label>
-            <input type="text" class="form-control" id="produto" name="qnt" value="" required="">
-          </div>
-        </div>
           <div class="col-md-12 d-flex flex-row-reverse">
             <button class="btn btn-primary" type="submit" id="salvar" name="salvar">salvar</button>
           </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
-  <div class="page-inner">
-  </div>
     <div class="card">
       <div class="card-header">
         <div class="card-body col-12 d-flex flex-row-reverse">
-          <form class="d-flex"action="" method="post">
-            <input  class="form-control"type="search" name="buscar" id="buscar" placeholder="Nome" aria-label="Search">
+          <form class="d-flex" action="" method="post">
+            <input class="form-control" type="search" name="buscar" id="buscar" placeholder="Nome" aria-label="Search">
             <button class="btn btn-primary m-1" type="submit">Pesquisar</button>
           </form>
         </div>
@@ -97,11 +98,13 @@ $result = $mysqli->query($SQL);
         </table>
       </div>
     </div>
+  </div>
 </body>
 <!-- Scripts -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/fd101988a9.js" crossorigin="anonymous"></script>
+
 </html>
 
 <script>
